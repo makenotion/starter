d1009052f530ec3faffd8d54ac9bb32796c67866
Sarah Lim <sarah@sarahlim.com>
2021-07-23 22:36:30 -0700

    Use full block data

--- pages/data.ts
+++ pages/data.ts
@@ -2,4 +2,53 @@ export const block = {
     id: "8947b61f-4386-4ead-ab52-00200a446140",
     version: 650,
     type: "page",
+    properties: {
+        LkiV: [["In Progress"]],
+        "%7%.": [["High Leverage"]],
+        ")fY@": [["6"]],
+        '99x"': [["‣", [["u", "7c74a801-7cb5-4914-bbbe-2b18cd1ced76"]]]],
+        "<fJl": [
+            ["‣", [["p", "2cb64ff2-4399-4346-8d71-e8f0b4de1964"]]],
+            [","],
+            ["‣", [["p", "4c70bf79-0c75-4caf-8d97-9d83388db374"]]],
+            [","],
+            ["‣", [["p", "4fccf201-b601-4daf-9a1d-0b8e6092533a"]]],
+            [","],
+            ["‣", [["p", "617bb3f1-273e-4013-83ba-c1d684dcb718"]]],
+            [","],
+            ["‣", [["p", "6bf3bb93-ea63-4a67-aa14-fb1a96319df0"]]],
+            [","],
+            ["‣", [["p", "d1717ca6-4e21-4f61-83da-ca21fd94c454"]]],
+        ],
+        "SFZ/": [
+            [
+                "‣",
+                [
+                    [
+                        "d",
+                        {
+                            type: "daterange",
+                            end_date: "2019-07-22",
+                            start_date: "2019-07-04",
+                        },
+                    ],
+                ],
+            ],
+        ],
+        "iz@s": [["⭐️⭐️"]],
+        jRZk: [["Integration,Scale"]],
+        title: [["API & Integrations"]],
+    },
+    content: [
+        "9daa89ef-29ac-495d-8963-50efd7f2abb6",
+        "8359066b-28da-46a3-bebc-7b6c01124ed6",
+        "8a2f32b3-3527-4211-9f18-a13410e137d4",
+    ],
+    created_by: "4b723c12-e06d-4354-9951-aef27cabf6f1",
+    created_time: 1535323282085,
+    last_edited_by: "7c74a801-7cb5-4914-bbbe-2b18cd1ced76",
+    last_edited_time: 1563929940000,
+    parent_id: "c91b8f67-c73b-41c4-a0a8-95a9faa0a98d",
+    parent_table: "collection",
+    alive: true,
 }
