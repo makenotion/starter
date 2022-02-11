0483ca231f8ee2614c2a07d9c128c58b63da94e5
Jordan Scales <jordan@makenotion.com>
2022-02-11 10:51:12 -0500

    :)

--- data.ts
+++ data.ts
@@ -1,22 +1,6 @@
 export const block = {
     id: "8947b61f-4386-4ead-ab52-00200a446140",
-    alive: true,
-    version: 900,
     title: "Hello, world!",
-    children: [
-        {
-            id: "9b5e6a35-405a-4a14-a830-64567adf3029",
-            alive: true,
-            version: 860,
-            title: "This is a paragraph",
-            children: [],
-        },
-        {
-            id: "0f7bb533-406f-4f50-83a2-9d8146dd0b43",
-            alive: true,
-            version: 220,
-            title: "This is another paragraph",
-            children: [],
-        },
-    ],
+    version: 4.0,
+    public: true,
 };
--- index.tsx
+++ index.tsx
@@ -1,9 +1,14 @@
+// @ts-nocheck
+// Feel free to remove if you'd like types!
+
 import { block } from "./data";
 
 export function App() {
   return (
     <pre>
-      <code>{JSON.stringify(block, null, 2)}</code>
+      <p>An empty React app to get you started! Edit index.tsx</p>
+      <p>◀ ▼ ▲ ▶</p>
+      {JSON.stringify(block, null, 2)}
     </pre>
   );
 }
