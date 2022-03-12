b3b2f499cced4dc9e5214532986b7c2a58398db4
Ryan Hart <ryan@makenotion.com>
2022-03-11 17:51:27 -0800

    Add squiggly braces to starter and add prettier.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
 // @ts-nocheck
 // Feel free to remove if you'd like types!
 
-import { block } from "./data";
+import { block } from "./data"
 
-console.log(block);
+console.log(block)
 
 export function App() {
-  return (
-    <main>
-      <p>An empty React app to get you started! Edit me in App.tsx</p>
-      <pre>
-        <p>◀ ▼ ▲ ▶</p>
-        <p>Check your browser console ⏭</p>
-      </pre>
-    </main>
-  );
+	return (
+		<main>
+			<p>An empty React app to get you started! Edit me in App.tsx</p>
+			<pre>
+				<p>
+					◀ ▼ ▲ ▶ {"{"} {"}"} {"["} {"]"}
+				</p>
+				<p>Check your browser console ⏭</p>
+			</pre>
+		</main>
+	)
 }
